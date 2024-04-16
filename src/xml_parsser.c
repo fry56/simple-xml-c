98f4baddf43b464d315240a33ceaf658b3e9b645
fry <j.danet@arka-studio.fr>
2024-04-16 12:56:48 +0200

    fix(codding style): *

--- src/xml_parsser.c
+++ src/xml_parsser.c
@@ -24,26 +24,37 @@ static int xml_is_valid_end_tag(char *buf, xml_node_t *current, size_t *index)
     return 2;
 }
 
+static bool skip_start_tag(char **buf, size_t *index)
+{
+    while ((*buf)[*index] != '<' && (*buf)[*index] != '\0') {
+        (*index)++;
+    }
+    return (*buf)[*index] != '\0';
+}
+
+static bool handle_end_tag(char *buf, xml_node_t **current, size_t *index)
+{
+    int valid_end = xml_is_valid_end_tag(buf, *current, index);
+
+    if (valid_end == 1) {
+        *current = (*current)->parent;
+        return true;
+    }
+    return valid_end == 0 ? false : true;
+}
+
 bool xml_parser(xml_t *doc, char *content)
 {
     size_t index = 0;
     char *buf = str_match(content, "<.*>");
     xml_node_t *current = NULL;
-    int valid_end;
 
     if (buf == NULL)
         return false;
     while (buf[index] != '\0') {
-        if (buf[index] != '<') {
-            index++;
-            continue;
-        }
-        valid_end = xml_is_valid_end_tag(buf, current, &index);
-        if (valid_end == 1) {
-            current = current->parent;
-            continue;
-        }
-        if (valid_end == 0)
+        if (!skip_start_tag(&buf, &index))
+            break;
+        if (!handle_end_tag(buf, &current, &index))
             return false;
         xml_add_data(doc, &current, &index, buf);
     }
